2f5dc5c42c65388a953a207d0ff008424c0d128d
Andrtisar <andrzey.sheremet@gmail.com>
2022-04-12 13:25:38 +0200

    VERY basic stuff on array

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -10,5 +10,9 @@ int main() {
 	for (int i = 0; i < 10; i++) {
 		std::cout << i << "\n";
 	}
+	int arr[3] = { 0, 2, 6 };
+	for (int i = 0; i < std::size(arr); ++i) {
+		std::cout << arr[i] << "\n";
+	}
 	return 0;
 }
