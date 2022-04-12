165dab0f4cbfa7e6a7f519e7f19c48903a27f4a8
Andrtisar <andrzey.sheremet@gmail.com>
2022-04-12 12:39:35 +0200

    Just some lööps

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -2,4 +2,13 @@
 
 int main() {
 	std::cout << "Hello World!";
+	std::cout << "\n++i\n";
+	for (int i = 0; i < 10; ++i) {
+		std::cout << i << "\n";
+	}
+	std::cout << "\ni++\n";
+	for (int i = 0; i < 10; i++) {
+		std::cout << i << "\n";
+	}
+	return 0;
 }
