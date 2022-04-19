dc09b72fbd291380263b99ac7dcd87221ea76c56
Andrtisar <andrzey.sheremet@gmail.com>
2022-04-19 13:18:49 +0200

    Fixed issue #3 - array length now makes more sense

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -1,18 +1,67 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+void calculateOccurs(int arr[], std::string line) {
+	for (int j = 0; j < line.length(); ++j) {
+		++arr[int(line[j]) - 32];
+	}
+}
+
 
 int main() {
-	std::cout << "Hello World!";
-	std::cout << "\n++i\n";
-	for (int i = 0; i < 10; ++i) {
-		std::cout << i << "\n";
+	// gets the path to file as an input from command line
+
+	std::string path;
+	std::ifstream input;
+
+
+	do {
+		std::cout << "Input file path, or write ! to exit:\n";
+		getline(std::cin, path);
+		if (path == "!") return 0;
+		input.open(path);
+	} while (!input.is_open());
+
+	//path = "Debug/test.txt";
+
+
+	// create an array filled with 0
+
+	int charOccurs[95];
+	for (int i = 0; i < 95; ++i) {
+		charOccurs[i] = 0;
 	}
-	std::cout << "\ni++\n";
-	for (int i = 0; i < 10; i++) {
-		std::cout << i << "\n";
+	// ASCII chars have numbers from 0 to 127, but only <32, 126> are printable,
+	// 32 - space, 126 - tilde
+	// which is why there are only 95 'counters'
+	// thus control chars are not counted
+
+
+	// reads the text from file
+
+	std::string line;
+	while (!input.eof()) {
+		getline(input, line);
+		// std::cout << line << std::endl;
+
+		// calculates the number of occurrences of each unique symbol
+		calculateOccurs(charOccurs, line);
 	}
-	int arr[3] = { 0, 2, 6 };
-	for (int i = 0; i < std::size(arr); ++i) {
-		std::cout << arr[i] << "\n";
+	input.close();
+
+	// outputs to another file
+
+	std::ofstream output;
+	output.open("output.txt");
+
+	std::cout << "Creating output.txt file with char occurences in input file\n";
+	for (int i = 0; i < 95; ++i) {
+		if (charOccurs[i]) {
+			// std::cout << char(i) << ": " << charOccurs[i] << std::endl;
+			output << char(i + 32) << ": " << charOccurs[i] << std::endl;
+		}
 	}
-	return 0;
+
+	output.close();
 }
