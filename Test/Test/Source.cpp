e10612d8c9128572ee5c6697df5433d4916ecc4a
Andrtisar <74014252+Andrtisar@users.noreply.github.com>
2022-04-22 10:47:58 +0200

    PRAC-4 implementing basic modularity (#7)
    Added methods for reading a file and outputting to a file

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -11,49 +11,30 @@ void calculateOccurs(int arr[], std::string line) {
 }
 
 
-int main() {
-	// gets the path to file as an input from command line
-
-	std::string path;
-	std::ifstream input;
-
-
-	do {
-		std::cout << "Input file path, or write ! to exit:\n";
-		getline(std::cin, path);
-		if (path == "!") return 0;
-		input.open(path);
-	} while (!input.is_open());
-
-	//path = "Debug/test.txt";
-
-
-	// create an array filled with 0
-
-	int charOccurs[possibleChars];
-	for (int i = 0; i < possibleChars; ++i) {
-		charOccurs[i] = 0;
-	}
-	// ASCII chars have numbers from 0 to 127, but only <32, 126> are printable,
-	// 32 - space, 126 - tilde
-
-
-	// reads the text from file
-
+bool calculateOccursInFile(std::string path, int arr[]) {
 	std::string line;
+	std::ifstream input;
+	input.open(path);
+	if (!input.is_open()) {
+		std::cout << "Could not open the file with a path " << path;
+		return false;
+	}
 	while (!input.eof()) {
 		getline(input, line);
 		// std::cout << line << std::endl;
 
 		// calculates the number of occurrences of each unique symbol
-		calculateOccurs(charOccurs, line);
+		calculateOccurs(arr, line);
 	}
 	input.close();
+	return true;
+}
+// so this is not really reusable, and I'm not sure if that can be fixed, or if it even is necessary
 
-	// outputs to another file
 
+void outputToFile(std::string outputPath, int arr[]) {
 	std::ofstream output;
-	output.open("output.txt");
+	output.open(outputPath);
 
 	std::cout << "Creating output.txt file with char occurences in input file\n";
 	for (int i = 0; i < possibleChars; ++i) {
@@ -62,6 +43,33 @@ int main() {
 			output << char(i) << ": " << charOccurs[i] << std::endl;
 		}
 	}
-  
 	output.close();
+}
+
+
+int main() {
+	// gets the path to file as an input from command line
+	std::string path;
+	std::cout << "Input file path:\n";
+	getline(std::cin, path);
+	// TODO - custom getline
+
+	//path = "Debug/test.txt";
+
+	
+	// create an array filled with 0
+	int charOccurs[possibleChars];
+	for (int i = 0; i < possibleChars; ++i) {
+		charOccurs[i] = 0;
+	}
+
+	// ASCII chars have numbers from 0 to 127, but only <32, 126> are printable,
+	// 32 - space, 126 - tilde
+
+	
+	// reads the text from file
+	if (calculateOccursInFile(path, charOccurs)) {
+		// outputs to another file
+		outputToFile("output.txt", charOccurs);
+	}
 }
