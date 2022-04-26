83c43550bbc397cba2de5d3e49e6f9c0612ac1f1
Andrtisar <andrzey.sheremet@gmail.com>
2022-04-26 17:04:30 +0200

    PRAC-9 made an advanced test with mock and such Muck No but like I seriously don't want to do this with pointers, even if it is possible

--- Test/NativeUnitTest/NativeUnitTest.cpp
+++ Test/NativeUnitTest/NativeUnitTest.cpp
@@ -2,7 +2,7 @@
 #include "CppUnitTest.h"
 #include "../Test/ConsoleReader.cpp"
 #include "../Test/Source.cpp"
-#include <iostream>
+#include <isolator.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,20 +10,50 @@ namespace NativeUnitTestForMain
 {
 	TEST_CLASS(MainTests)
 	{
+		std::string path = "D:/bruhmoment/Abyss/test.txt";
+		int charOccurs[possibleChars] = {};
 	public:
 		
-		TEST_METHOD(TestIfPathExists)
+		TEST_METHOD(TestIfInputPathExists)
 		{
-			int charOccurs[possibleChars] = {};
-			std::string path = "D:/bruhmoment/Abyss/ToDo.txt";
+			Assert::IsTrue(calculateOccursInFile(path, charOccurs));
+		}
+		TEST_METHOD(TestIfOutputPathExists)
+		{
 			Assert::IsTrue(calculateOccursInFile(path, charOccurs));
 		}
 		TEST_METHOD(TestCalculateOccursInLine) 
 		{
-			int charOccurs[possibleChars] = {};
 			std::string line = "aaA  ";
 			calculateOccurs(charOccurs, line);
 			Assert::AreEqual(2, charOccurs[int('a')]);
+		}
+		TEST_METHOD(TestConsoleReader)
+		{
+			// ConsoleReader* reader = FAKE<ConsoleReader>();
+			ConsoleReader reader;
+			FAKE_GLOBAL(_getch);
+			char testChars[] = {'a', 'b', 8, 13};
+
+			// WHEN_CALLED(_getch()).Return(int(testChars[++i]));
+			// int num = TIMES_CALLED(_getch()) - 1;
+			//(TIMES_CALLED(_getch()))
+
+			WHEN_CALLED(_getch()).Return(testChars[0]);
+			WHEN_CALLED(_getch()).Return(testChars[1]);
+			WHEN_CALLED(_getch()).Return(testChars[2]);
+			WHEN_CALLED(_getch()).Return(testChars[3]);
+			reader.readConsole("test");
+			Assert::AreEqual(1, int(reader.getData().length()));
+			// I don't want to do this with pointers ;(
+		}
+		/*TEST_METHOD(TestCalculateOccursInFile)
+		{
+
+		}*/
+		TEST_METHOD_CLEANUP(TearDown)
+		{
+			ISOLATOR_CLEANUP();
 		}
 	};
 }
